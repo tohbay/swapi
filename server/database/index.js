122a198d3475fa5ef3faaef0f2a9654686bbf29e
tobechukwuobitube <tobechukwuobitube@gmail.com>
2020-02-14 12:25:02 +0100

    setup database connections

---
+++ server/database/index.js
@@ -0,0 +1,46 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const CONNECTION_STRING =
+  process.env.DATABASE_URL || process.env.DATABASE_DEV_URL;
+
+const SSL = process.env.NODE_ENV === 'production';
+
+class Database {
+  constructor() {
+    this._pool = new Pool({
+      connectionString: CONNECTION_STRING,
+      ssl: SSL
+    });
+
+    this._pool.on('error', (err, client) => {
+      console.error('Unexpected error on idle PostgreSQL client.', err);
+      process.exit(-1);
+    });
+  }
+
+  query(query, ...args) {
+    this._pool.connect((err, client, done) => {
+      if (err) throw err;
+      const params = args.length === 2 ? args[0] : [];
+      const callback = args.length === 1 ? args[0] : args[1];
+
+      client.query(query, params, (err, res) => {
+        done();
+        if (err) {
+          console.log(err.stack);
+          return callback({ error: 'Database error.' }, null);
+        }
+        callback({}, res.rows);
+      });
+    });
+  }
+
+  end() {
+    this._pool.end();
+  }
+}
+
+export default new Database();
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import db from './database';
+import movies from './database/swapi-data/movies.json';
+import characters from './database/swapi-data/characters.json';
+
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +20,12 @@ app.get('/', function(req, res) {
 
 app.listen(PORT, function() {
   console.log(`App is listening on Port ${PORT}`);
+  console.log(movies.count, characters.count);
+});
+
+db.query('SELECT Now()', (err, res) => {
+  if (err.error) return console.log(err.error);
+  console.log(`PostgreSQL connected: ${res[0].now}.`);
 });
 
 export default app;
